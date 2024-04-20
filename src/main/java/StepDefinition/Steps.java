d6eb0ca9aed75378f2c136387106e5ee021944f4
Tahsin <tahsinsyeed7@hotmail.com>
2024-04-19 22:38:44 -0400

    version 2

--- src/main/java/StepDefinition/Steps.java
+++ src/main/java/StepDefinition/Steps.java
@@ -4,25 +4,62 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+public class Steps {
 
-	public class Steps {
-		
-		@Given("^a precondition is valid$")
-		public void a_precondition_is_valid() {
-		    System.out.println("User1 logs in");
-		}
-
-		@When("^an action is performed$")
-		public void an_action_is_performed(){
-			  System.out.println("User1 clicks button");
-		}
-
-		@Then("^something should be asserted$")
-		public void something_should_be_asserted()  {
-			 System.out.println("User1 is re-direced to main page");
-		}
-
-
+	@Given("{string} navigates to homepage")
+	public void navigates_to_homepage(String string) {
+		System.out.println(string + " navigates to homapage");
 
 	}
 
+	@When("User enters username as {string}")
+	public void user_enters_username_as(String string) {
+
+		System.out.println(string + " enters username");
+
+	}
+
+	@When("User enters password as {string}")
+	public void user_enters_password_as(String string) {
+		System.out.println(string + " enters password");
+	}
+
+	@When("clicks login button")
+	public void clicks_login_button() {
+
+	}
+
+	@Then("User is able to login to the MyProfile page")
+	public void user_is_able_to_login_to_the_my_profile_page() {
+
+	}
+
+	@Then("User clicks on logout")
+	public void user_clicks_on_logout() {
+
+	}
+
+	@When("UserABCD types username as {string}")
+	public void user_abcd_types_username_as(String string) {
+		System.out.println("Userabcd enters username as " + string);
+	}
+
+	@When("UserABCD types password as {string}")
+	public void user_abcd_types_password_as(String string) {
+		System.out.println("userAB enters password as " + string);
+
+	}
+
+	@When("clicks login buttons")
+	public void clicks_login_buttons() {
+
+	}
+
+	@Then("User is not able to login to the MyProfile page")
+	public void user_is_not_able_to_login_to_the_my_profile_page() {
+
+		System.out.println("fail");
+
+	}
+
+}
--- target/classes/featureFile/MyFeature.feature
+++ target/classes/featureFile/MyFeature.feature
@@ -1,6 +1,17 @@
-Feature: Post method
+Feature: Login function
 
-  Scenario: scenario description
-    Given a precondition is valid
-    When an action is performed
-    Then something should be asserted
+  Background: User is in Homepage
+    Given "Tahsin" navigates to homepage
+
+  Scenario: User login
+    When User enters username as "T"
+    And User enters password as "password"
+    And clicks login button
+    Then User is able to login to the MyProfile page
+    And User clicks on logout
+
+  Scenario: User false login
+    When UserABCD types username as "A"
+    And UserABCD types password as "password123"
+    And clicks login buttons
+    Then User is not able to login to the MyProfile page
